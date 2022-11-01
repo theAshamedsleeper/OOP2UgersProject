e3e3b5aca76ac00c8118624b5ee3eeb0674f351b
theAshamedsleeper <thorbjorn114@gmail.com>
2022-11-01 11:15:03 +0100

    player klass

---
+++ 2UgersProject Stardew Valley/2UgersProject Stardew Valley/Player.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System.Net.NetworkInformation;
+
+namespace _2UgersProject_Stardew_Valley
+{
+    internal class Player : GameObjects
+    {
+        private float energy;
+        private float hunger;
+
+        public Player(Vector2 pos) : base(pos)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+        private void HandleInput()
+        {
+
+        }
+        private void Eat()
+        {
+
+        }
+        private void OnCollision(GameObjects other)
+        {
+
+        }
+        protected void LoadContent()
+        {
+
+        }
+    }
+}
