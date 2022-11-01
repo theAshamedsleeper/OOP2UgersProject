be00a26b40897d3749fc269fdac8d5331a6f3d4f
Sky1pigeon <112692970+Sky1pigeon@users.noreply.github.com>
2022-11-01 11:41:58 +0100

    at

--- 2UgersProject Stardew Valley/2UgersProject Stardew Valley/Terrain.cs
+++ 2UgersProject Stardew Valley/2UgersProject Stardew Valley/Terrain.cs
@@ -58,19 +58,23 @@ namespace _2UgersProject_Stardew_Valley
             return 0;
         }
         #endregion
-        public static void LoadContent(content: ContentManager)
+        #region LoadUpdateDraw
+        public static void LoadContent(ContentManager content)
         {
 
         }
-        public static void Update(gametime: GameTime)
+        public static void Update(GameTime gametime)
         {
 
         }
-        public static void Draw(spritebach: SpriteBatch)
+        public static void Draw(SpriteBatch spritebatch)
         {
+            for (int i = 0; i < width * height; i++)
+            {
 
+            }
         }
-        private static void Animate(gametime: GameTime)
+        private static void Animate(GameTime gametime)
         {
 
         }
@@ -78,6 +82,7 @@ namespace _2UgersProject_Stardew_Valley
         {
 
         }
+        #endregion
         private static void Decrease_Speed()
         {
 
@@ -86,9 +91,36 @@ namespace _2UgersProject_Stardew_Valley
         {
 
         }
-        public static string X
+        #region getinfo
+        public static int X
         {
-            get { return ; }
+            get { return tiles_x[0]; }
         }
+        static int Which_Terrain(int[] pos, int[] tx, int[] ty, int[] t)
+        {
+            // posision of a given input, and returns the position of where in the array of briks, that is the given value of the coordinate of the input.
+            // checks where in array of y coordinates, the given inpus is.
+            int y_1 = pos[1] - pos[1] % 32;
+            int x_1 = pos[0] - pos[0] % 32;
+            int x_mod = 0;
+            /* it locates the correct pos in the array by going down 8 times in x by each y value, as 8 is then the y value increases.
+            * and the coordinate system could be written in one array, where each pos is x, up to 64, so i return it to that. */
+            for (int i = 0; i < height; i++)
+            {
+                if (ty[i * width] == y_1)
+                {
+                    x_mod = i;
+                }
+            }
+            for (int i = 0; i < width; i++)
+            {
+                if (tx[(x_mod * width) + i] == x_1)
+                {
+                    return t[(x_mod * width) + i];
+                }
+            }
+            return 0;
+        }
+        #endregion
     }
 }
